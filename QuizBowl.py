569b3de797f130ab9f873b19a5229f345ca33c73
Aidan945 <70586929+Aidan945@users.noreply.github.com>
2022-10-26 09:33:14 -0500

    Update QuizBowl.py
    Added core question + answer algorithm to check if the user's answer is correct/incorrect and add points accordingly. Need to add input of sound file etc.

--- QuizBowl.py
+++ QuizBowl.py
@@ -12,17 +12,25 @@ teams_points = []
 
 for teams in range(number_of_teams):
     teams_points.append(0)
-    teams_trueorfalse.append(False)
+    # teams_trueorfalse.append(False)
 
 
 for team in range(number_of_teams):
     input("team "+ str(team+1) +" is up, press enter when you are ready")
 
-    
+    answer = int(input("what is 2+2?"))
+    correct_answer = 4
+    if answer == correct_answer:
+        teams_points[team] += 10
+        print("you got the question right first try!")
+    else: # re ask question
+        print("you got the question wrong try again!")
+        answer = int(input("what is 2+2?"))
+        if answer == correct_answer:
+            teams_points[team] += 5
+            print("you got the question right second try!")
+        else:
+            print("you got the question wrong both tries!")    
 
 
-print(teams_trueorfalse)
 print(teams_points)
-
-
-
