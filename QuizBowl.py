c5d0155cfc0ff3191e527e5c535d2c5d1a1c2668
Aidan945 <70586929+Aidan945@users.noreply.github.com>
2022-11-01 09:04:27 -0500

    Update QuizBowl.py
    Added code to allow multiple questions to be asked.

--- QuizBowl.py
+++ QuizBowl.py
@@ -1,7 +1,8 @@
-import os, csv
+import os, csv, random
 os.system('cls')
 
 number_of_teams = int(input("How many teams are there? "))
+number_of_questions = int(input("How many questions does each team want to answer? "))
 
 teams_trueorfalse = []
 teams_points = []
@@ -9,21 +10,24 @@ teams_points = []
 for teams in range(number_of_teams):
     teams_points.append(0)
 
-for team in range(number_of_teams):
-    input("Team "+ str(team+1) +" is up, press enter when you are ready.")
-
-    answer = int(input("what is 2+2?"))
-    correct_answer = 4
-    if answer == correct_answer:
-        teams_points[team] += 10
-        print("You got the question right on the first try!")
-    else:
-        print("You got the question wrong. Try again!")
-        answer = int(input("what is 2+2?"))
-        if answer == correct_answer:
-            teams_points[team] += 5
-            print("You got the question right on the second try!")
+first_number = 4
+second_number = 3
+for question in range(number_of_questions):
+    for team in range(number_of_teams):
+        input("Team "+ str(team+1) +" is up. Please click enter once you are ready.")
+        answer = int(input("what is "+str(first_number)+"+"+str(second_number)+"? "))
+        if answer == second_number + first_number:
+            teams_points[team] += 10
+            print("You got the question right on the first try!")
         else:
-            print("You got the question wrong on both tries!")    
+            print("You got the question wrong. Try again!")
+            answer = int(input("what is "+str(first_number)+"+"+str(second_number)+"? "))
+            if answer == second_number + first_number:
+                teams_points[team] += 5
+                print("You got the question right on the second try!")
+            else:
+                print("You got the question wrong on both tries!")
+        first_number += random.randint(2,7)
+        second_number += random.randint(2,7)    
 
 print(teams_points)
