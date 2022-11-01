21ab4ca866392e0fa87ce4295be0fe6635abf1d7
sahad24greenhill <112664636+sahad24greenhill@users.noreply.github.com>
2022-11-01 08:38:59 -0500

    Update QuizBowl.py

--- QuizBowl.py
+++ QuizBowl.py
@@ -1,36 +1,29 @@
 import os, csv
 os.system('cls')
 
-#  Create the teams (using a dictionary)
-#  
-
-number_of_teams = int(input("how many teams are there?"))
-
+number_of_teams = int(input("How many teams are there? "))
 
 teams_trueorfalse = []
 teams_points = []
 
 for teams in range(number_of_teams):
     teams_points.append(0)
-    # teams_trueorfalse.append(False)
-
 
 for team in range(number_of_teams):
-    input("team "+ str(team+1) +" is up, press enter when you are ready")
+    input("Team "+ str(team+1) +" is up, press enter when you are ready.")
 
     answer = int(input("what is 2+2?"))
     correct_answer = 4
     if answer == correct_answer:
         teams_points[team] += 10
-        print("you got the question right first try!")
+        print("You got the question right on the first try!")
     else: # re ask question
-        print("you got the question wrong try again!")
+        print("You got the question wrong. Try again!")
         answer = int(input("what is 2+2?"))
         if answer == correct_answer:
             teams_points[team] += 5
-            print("you got the question right second try!")
+            print("You got the question right on the second try!")
         else:
-            print("you got the question wrong both tries!")    
-
+            print("You got the question wrong on both tries!")    
 
 print(teams_points)
